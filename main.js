bdd8d1db6f0de426f1b061be382fec93a287b2ac
Daria Tabala <dasha.tabala@gmail.com>
2019-10-26 20:24:08 +0300

    wip

--- main.js
+++ main.js
@@ -1,49 +1,78 @@
+const nav = document.querySelector('nav')
+const div = document.querySelector('.container')
+const list = document.createElement('UL')
+nav.appendChild(list)
 
-const content = document.querySelector('.content')
-const  nav = document.querySelector('nav')
 
-const bookList = document.createElement('UL')
-bookList.classList.add('list')
-//add to our ul  - li
-const addListElements = function (arr) {
-  arr.map(function (el) {
-    const bookListElement = document.createElement('LI')
-    const bookListTitle = document.createTextNode(el.title)
 
-    bookListElement.classList.add('book-list-element');
-    bookListElement.dataset.index = arr.indexOf(el);
-    bookListElement.appendChild(bookListTitle);
-    bookList.appendChild(bookListElement);
+const addElement = function (storage) {
+  storage.map(function (i) {
+    const listElement = document.createElement('LI')
+    const listTitle = document.createTextNode(i.author)
+    listElement.classList.add('book-list')
+
+    // const vi = document.createElement('DIV')
+    // vi.classList.add('soned')
+    // listElement.appendChild(vi)
+    listElement.appendChild(listTitle)
+    list.appendChild(listElement)
   })
-};
-//call to create list of all title 
-addListElements(storage);
-//add active class to the classList
-nav.appendChild(bookList).firstChild.classList.add('active')
-//add content to the li
-const addContent = function(data){
-  content.innerHTML = `
-<h2>${data.title}</h2>
-<p class="music-price">Price: ${data.price}</p>
-<p>${data.description}</p>
-<img src="${data.imgPath}" alt="${data.title}" class="music-img">
-`
-};
+}
+addElement(storage)
+// const listElement = document.createElement('LI')
+// const listTitle = document.createTextNode(i.title)
+// listElement.classList.add('book-list')
+// listElement.dataset.index = bookList.indexOf(i)
+// listElement.appendChild(listTitle)
+// list.appendChild(listElement)
+// addElement(storage)
 
-addContent(storage[0]);
+nav.appendChild(list).firstChild.classList.add('active')
 
-nav.addEventListener('click', event => {
-  let navElement = event.target;
+const creatContent = function (data) {
+  div.innerHTML = `
+  <h2>${data.author}</h2>
+      <h2>${data.title}</h2>
+    <p>${data.about}</p>
+    <span> ${data.price}</span>
+    <img src="${data.cover}" alt="${data.title}" class="cover-book">
+    `
+}
+creatContent(storage[0])
+// function color(){
+//   this.style.color="red";
+//   }
+  
+//   var mas =document.getElementsByTagName("LI");
+//   for(i=0; i<mas.length;i++)
+//   mas[i].addEventListener("click", color, false);
+      
 
-  if (document.querySelector('.active')) {
-    document.querySelector('.active').classList.remove('active');
-  }
+// // creatContent(storage[0])
 
-  if (navElement.tagName != 'LI') {
-    return;
-  }
+// // const changeText = function () {
+// //   for (let i = 0; i < storage.length; i++) {
+// //     addElement(storage[i])
+// //   }
 
-  navElement.classList.add('active');
-  let bookShop = storage[navElement.dataset.index];
-  addContent(bookShop);
-});
+// // }
+
+
+  nav.addEventListener('click', function(event){
+    let navElement = event.target;
+
+    if (document.querySelector('.active')) {
+        document.querySelector('.active').classList.remove('active');
+    }
+    if (navElement.tagName != 'LI') {
+        return;
+    }
+    navElement.classList.add('active');
+      creatContent(storage[i])
+  
+    })
+  
+
+
+
+
--- storage.js
+++ storage.js
@@ -1,33 +1,38 @@
 
 const storage = [
     {
-        title: 'Eat That Frog by Brian Tracy',
+        author: 'Brian Tracy',
+        title: 'Eat That Frog',
+        about: 'Want to do more than just get more done? How about get what matters done so that you are making quantum leaps in your business? This book makes too much sense and has changed the way I plan and prioritize my day.  Put it this way, I eat my frog in the morning, and I focus on the top three things that only I can do in my business a whole lot more.  Excellent read! (thanks Tonya Davidson for recommending).',
         price: '$18.69 Hardcover',
-        description: 'Want to do more than just get more done? How about get what matters done so that you are making quantum leaps in your business? This book makes too much sense and has changed the way I plan and prioritize my day.  Put it this way, I eat my frog in the morning, and I focus on the top three things that only I can do in my business a whole lot more.  Excellent read! (thanks Tonya Davidson for recommending).',
-        imgPath: 'src/book_frog.jpg'
+        cover: 'src/book_frog.jpg'
     },
     {
+        author: "Chris Brogan and Julien Smith",
         title: 'The Impact Equation by Chris Brogan and Julien Smith',
+        about: 'This is not another just another social media book but rather a book about connections and relationships. I always enjoy what Chris and Julien have to say and this book did not disappoint.  They have a way of writing that makes you feel like they totally get you, and are talking with you.  This was the perfect complement to Trust Agents which I also really enjoyed.',
         price: '$5.89 Hardcover',
-        description: 'This is not another just another social media book but rather a book about connections and relationships. I always enjoy what Chris and Julien have to say and this book did not disappoint.  They have a way of writing that makes you feel like they totally get you, and are talking with you.  This was the perfect complement to Trust Agents which I also really enjoyed.',
-        imgPath: 'src/book_impact.jpg'
+        cover: 'src/book_impact.jpg'
     },
     {
+        author: "Carol S. Dweck",
         title: 'Mindset: The New Psychology of Success',
+        about: 'After decades of research, world-renowned Stanford University psychologist Carol S. Dweck, Ph.D., discovered a simple but groundbreaking idea: the power of mindset. In this brilliant book, she shows how success in school, work, sports, the arts, and almost every area of human endeavor can be dramatically influenced by how we think about our talents and abilities. People with a fixed mindset — those who believe that abilities are fixed — are less likely to flourish than those with a growth mindset — those who believe that abilities can be developed. Mindset reveals how great parents, teachers, managers, and athletes can put this idea to use to foster outstanding accomplishment.',
         price: '$10.26 Paperback',
-        description: 'After decades of research, world-renowned Stanford University psychologist Carol S. Dweck, Ph.D., discovered a simple but groundbreaking idea: the power of mindset. In this brilliant book, she shows how success in school, work, sports, the arts, and almost every area of human endeavor can be dramatically influenced by how we think about our talents and abilities. People with a fixed mindset — those who believe that abilities are fixed — are less likely to flourish than those with a growth mindset — those who believe that abilities can be developed. Mindset reveals how great parents, teachers, managers, and athletes can put this idea to use to foster outstanding accomplishment.',
-        imgPath: 'src/book_mindset.jpg'
+        cover: 'src/book_mindset.jpg'
     },
     {
+        author: "Mortimer J. Adler, Charles Van Doren",
         title: 'How to Read a Book: The Classic Guide to Intelligent Reading',
+        about: 'How to Read a Book, originally published in 1940, has become a rare phenomenon, a living classic. It is the best and most successful guide to reading comprehension for the general reader. And now it has been completely rewritten and updated. You are told about the various levels of reading and how to achieve them – from elementary reading, through systematic skimming and inspectional reading, to speed reading, you learn how to pigeonhole a book, X-ray it, extract the author\'s message, criticize. You are taught the different reading techniques for reading practical books, imaginative literature, plays, poetry, history, science and mathematics, philosophy and social science.',
         price: '$11.99',
-        description: 'YouTube Music is a music streaming service and mobile app developed by YouTube; it provides a tailored interface for the service oriented towards music streaming, allowing users to browse through songs and music videos on YouTube based on genres, playlists, and recommendations. The service also offers a premium tier, which enables ad-free playback, audio-only background playback, and downloading songs for offline playback. These subscription benefits are also offered to subscribers of Play Music and YouTube Premium.',
-        imgPath: 'src/book_how.jpg'
+        cover: 'src/book_how.jpg'
     },
     {
+        author: "Susan Wise Bauer",
         title: 'The Well-Educated Mind: A Guide to the Classical Education You Never Had',
+        about: 'Have you lost the art of reading for pleasure? Are there books you know you should read but haven\'t because they seem too daunting? In The Well-Educated Mind, Susan Wise Bauer provides a welcome and encouraging antidote to the distractions of our age, electronic and otherwise. In her previous book, The Well-Trained Mind, the author provided a road map of classical education for parents wishing to home-school their children, and that book is now the premier resource for home-schoolers. In this new book, Bauer takes the same elements and techniques and adapts them to the use of adult readers who want both enjoyment and self-improvement from the time they spend reading.',
         price: '$18.32 Hardcover',
-        description: 'Have you lost the art of reading for pleasure? Are there books you know you should read but haven\'t because they seem too daunting? In The Well-Educated Mind, Susan Wise Bauer provides a welcome and encouraging antidote to the distractions of our age, electronic and otherwise. In her previous book, The Well-Trained Mind, the author provided a road map of classical education for parents wishing to home-school their children, and that book is now the premier resource for home-schoolers. In this new book, Bauer takes the same elements and techniques and adapts them to the use of adult readers who want both enjoyment and self-improvement from the time they spend reading.',
-        imgPath: 'src/book_educated.jpg'
+        cover: 'src/book_educated.jpg'
     }
 ];
